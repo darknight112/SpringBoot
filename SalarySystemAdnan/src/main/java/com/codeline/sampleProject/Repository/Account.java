b56e7d3031fcc96b157745067796e94f6aca99ae
darknight112 <hkalamri@gmail.com>
2023-05-19 10:18:07 +0400

    update

--- SalarySystemAdnan/src/main/java/com/codeline/sampleProject/Repository/Account.java
+++
@@ -1,8 +0,0 @@
-package com.codeline.sampleProject.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Account extends JpaRepository<Account, Long> {
-}
