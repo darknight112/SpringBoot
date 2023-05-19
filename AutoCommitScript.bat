c53968c565e5d6492bfb7d6689384186438750ae
darknight112 <hkalamri@gmail.com>
2023-05-19 10:54:33 +0400

    Automatic commit

---
+++ AutoCommitScript.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd C:\Users\hkala\OneDrive\Desktop\Springboot\SpringBoot  # Replace with the path to your repository
+
+# Add your files to the repository
+git add .
+
+# Commit the changes
+git commit -m "Automatic commit"
+
+# Push the changes to GitHub
+git push
